b169c945bc458e984579c7b98202d9eca5b1b763
Pooja shelake <75307225+pooja-shelke@users.noreply.github.com>
2021-08-04 14:18:43 +0530

    Quetions on File

--- Program_4.c
+++ Program_4.c
@@ -3,99 +3,47 @@
 #include<unistd.h>
 #include<fcntl.h>
 
-struct student
+int Display(char fname[],char ch)
 {
-    int Rollno;
-    char Name[20];
-    int Marks;
-};
-
-void FileWrite(char *name)
-{
-    int fd = 0, ret = 0, size = 0, i = 0;
-    struct student sobj;
-    
-    fd = open(name,O_WRONLY);
-    if(fd == -1)
-    {
-        printf("Unable to open file\n");
-        return ;
-    }
-    printf("Enter number of students");
-    scanf("%d",&size);
-    
-    for(i = 1; i <= size; i++)     
-    {
-        printf("Enter roll number\n");
-        scanf("%d",&sobj.Rollno);
-        printf("Enter Name of student\n");
-        scanf("%s",&sobj.Name);
-        printf("Enter marks\n");
-        scanf("%d",&sobj.Marks);
-        
-        write(fd,&sobj,sizeof(sobj));  
-    }
-    close(fd);
+	int fd=0;
+	char arr[100];
+	int ret=0;
+	int iCnt=0,i=0;
+	fd=open(fname,O_RDONLY);
+	
+	if(fd==-1)
+	{
+		printf("Unable to open");
+		return -1;
+	}
+	
+	while((ret=read(fd,arr,sizeof(arr)))!=0)
+	{
+		for(i=0;i<ret;i++)
+		{
+			if(arr[i]==ch)
+			{
+				iCnt++;
+			}
+		}
+	}
+    return iCnt;
+	close(fd);	
 }
-
-void FileRead(char *name)
-{
-    int fd = 0, ret = 0, size = 0, i = 0;
-    struct student sobj;
-    
-    fd = open(name,O_RDONLY);
-    if(fd == -1)
-    {
-        printf("Unable to open file\n");
-        return ;
-    }
-    
-    printf("Data from file is : \n");
-    
-    while((ret = read(fd,&sobj,sizeof(sobj)))!= 0)
-    {
-        printf("Marks : %d\n",sobj.Marks);
-    }
-    
-    close(fd);
-}
-
-
 int main()
 {
-    char name[20];
-    
-    printf("Enter file name\n");
-    scanf("%s",name);
-    
-   FileWrite(name);
-    
-    FileRead(name);
-    
-    return 0;
+	char fname[30];
+	int ret=0;
+	char ch='\0';
+	printf("Enter file name:");
+	scanf("%s",fname);
+	
+	printf("Enter character:");
+	scanf(" %c",&ch);
+	
+	ret=Display(fname,ch);
+	
+	printf("Number of freaquency of character is is%d",ret);
+	
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Program_5.c
+++ Program_5.c
@@ -1,108 +1,43 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<fcntl.h>
 
-struct student
+void File(char name[],int size)
 {
-    int Rollno;
-    char Name[20];
-    int Marks;
-};
-
-void FileWrite(char *name)
-{
-    int fd = 0, ret = 0, size = 0, i = 0;
-    struct student sobj;
-    
-    fd = open(name,O_WRONLY);
-    if(fd == -1)
-    {
-        printf("Unable to open file\n");
-        return ;
-    }
-    printf("Enter number of students");
-    scanf("%d",&size);
-    
-    for(i = 1; i <= size; i++)     
-    {
-        printf("Enter roll number\n");
-        scanf("%d",&sobj.Rollno);
-        printf("Enter Name of student\n");
-        scanf("%s",&sobj.Name);
-        printf("Enter marks\n");
-        scanf("%d",&sobj.Marks);
-        
-        write(fd,&sobj,sizeof(sobj));  
-    }
-    close(fd);
-}
-
-int FileRead(char *name)
-{
-    int fd = 0, ret = 0, size = 0, i = 0,iSum=0;
-    struct student sobj;
-   
-    fd = open(name,O_RDONLY);
-    if(fd == -1)
-    {
-        printf("Unable to open file\n");
-        return -1;
-    }
-    
-    printf("Data from file is : \n");
-    
-    while((ret = read(fd,&sobj,sizeof(sobj)))!= 0)
-    
-    for(i=0;i<=ret;i++)
+	int fd=0,ret=0;
+	char *arr=NULL;
+	arr=(char *)malloc(size);
+	fd=open(name,O_RDONLY);
+	
+	if(fd==-1)
 	{
-		
-		iSum=iSum+sobj.Marks;
+		printf("Unable to open");
+		return;
 	}
-//printf("Addition is:%d",iSum);
-
-return iSum;
-    close(fd);
+	
+	ret=read(fd,arr,size);
+	
+		write(1,arr,ret);
+	
+	
+	close(fd);
+	free(arr);
 }
 
 
 int main()
 {
-    char name[20];
-    int ret=0;
-    printf("Enter file name\n");
-    scanf("%s",name);
-    
-   FileWrite(name);
-    
-   ret= FileRead(name);
-   
-   printf("Addition is:%d",ret);
-    
-    return 0;
+	char name[30];
+	int size=0;
+	printf("Enter file name:");
+	scanf(" %s",&name);
+	
+	printf("Enter size:");
+	scanf("%d",&size);
+	
+	File(name,size);
+	
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
